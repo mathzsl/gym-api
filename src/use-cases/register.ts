1ce050f388e77e3976459d3805c9d5cf67a18dad
math-72 <matheus.soares@dcx.ufpb.br>
2025-10-12 14:50:19 -0300

    feat: implement user registration functionality with validation and hashing

---
+++ src/use-cases/register.ts
@@ -0,0 +1,27 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { hash } from "bcryptjs";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6);
+
+    const emailAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyExists) {
+      throw new Error("Email already exists.");
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+  }
+}
